d3499595bec564e2b7b9856c0dbee8e036c31b9b
Adithya B Shetty <adithyabshetty100@gmail.com>
2021-06-22 10:00:02 +0530

    changed jsx to js

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -6,11 +6,11 @@ function Pokecard(props){
     let url=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`;
 
     return(
-        <div>
+        <div class="card">
             <p className="name">{props.name}</p> 
-            <img src={url} alt="" className="imgURL" />
-            <p className="type">{props.type}</p>
-            <p className="exp">{props.exp}</p>
+            <img src={url} alt={props.name} className="imgURL" />
+            <p className="type">Type: {props.type}</p>
+            <p className="exp">EXP: {props.exp}</p>
         </div>
     )
 }
